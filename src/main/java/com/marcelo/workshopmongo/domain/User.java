b939e4b6cf303f66aa1e3aad2028e5c08b50e4ec
K1dou <hique1276@gmail.com>
2024-03-03 14:00:44 -0300

    Referenciando os posts do usuário

--- src/main/java/com/marcelo/workshopmongo/domain/User.java
+++ src/main/java/com/marcelo/workshopmongo/domain/User.java
@@ -1,10 +1,13 @@
 package com.marcelo.workshopmongo.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -19,6 +22,9 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    private List<Post>posts=new ArrayList<>();
 
     public User() {
     }
@@ -52,6 +58,13 @@ public class User implements Serializable {
     public void setEmail(String email) {
         this.email = email;
     }
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -65,4 +78,6 @@ public class User implements Serializable {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+
 }
