60f0d581da0ba556ef7c7a266de2f56b1f5fe608
K1dou <hique1276@gmail.com>
2024-03-03 14:21:08 -0300

    Obtendo um post por id

---
+++ src/main/java/com/marcelo/workshopmongo/resources/PostResource.java
@@ -0,0 +1,30 @@
+package com.marcelo.workshopmongo.resources;
+
+import com.marcelo.workshopmongo.domain.Post;
+import com.marcelo.workshopmongo.domain.User;
+import com.marcelo.workshopmongo.dto.UserDTO;
+import com.marcelo.workshopmongo.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService postService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findbyId(@PathVariable String id) {
+        Post obj = postService.findById(id);
+
+        return ResponseEntity.ok().body(obj);
+    }
+
+
+
+}
--- src/main/java/com/marcelo/workshopmongo/services/PostService.java
+++ src/main/java/com/marcelo/workshopmongo/services/PostService.java
@@ -1,14 +1,28 @@
 package com.marcelo.workshopmongo.services;
 
+import com.marcelo.workshopmongo.domain.Post;
+import com.marcelo.workshopmongo.domain.User;
 import com.marcelo.workshopmongo.repository.PostRepository;
+import com.marcelo.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Service
 public class PostService {
 
     @Autowired
     private PostRepository postRepository;
+
+
+    public Post findById(String id) {
+        try {
+            return postRepository.findById(id).get();
+        } catch (NoSuchElementException e) {
+            throw new ObjectNotFoundException("Id nao encontrado!");
+        }
+    }
 
 
 
