162386c1b34d7a35ebdab2e2d02f14ce992e71d8
K1dou <hique1276@gmail.com>
2024-03-03 14:05:18 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/marcelo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/marcelo/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.marcelo.workshopmongo.resources;
 
+import com.marcelo.workshopmongo.domain.Post;
 import com.marcelo.workshopmongo.domain.User;
 import com.marcelo.workshopmongo.dto.UserDTO;
 import com.marcelo.workshopmongo.services.UserService;
@@ -60,6 +61,12 @@ public class UserResource {
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
     }
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
 
+        return ResponseEntity.ok().body(obj.getPosts());
+
+    }
 
 }
